1503a9eab1d3669f48dde7840221a1f418028794
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-29 21:16:34 +0000

    Deploy rust-lang/chalk to github.com/rust-lang/chalk.git:gh-pages

---
+++ implementors/chalk_engine/context/trait.ResolventOps.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["chalk_solve"] = [{text:"impl <a class=\"trait\" href=\"chalk_engine/context/trait.ResolventOps.html\" title=\"trait chalk_engine::context::ResolventOps\">ResolventOps</a>&lt;<a class=\"struct\" href=\"chalk_solve/solve/slg/struct.SlgContext.html\" title=\"struct chalk_solve::solve::slg::SlgContext\">SlgContext</a>&gt; for <a class=\"struct\" href=\"chalk_solve/solve/slg/struct.TruncatingInferenceTable.html\" title=\"struct chalk_solve::solve::slg::TruncatingInferenceTable\">TruncatingInferenceTable</a>",synthetic:false,types:["chalk_solve::solve::slg::TruncatingInferenceTable"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["chalk"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"chalk/lowering/enum.RustIrError.html\" title=\"enum chalk::lowering::RustIrError\">RustIrError</a>",synthetic:false,types:["chalk::lowering::RustIrError"]},];
+implementors["failure"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
